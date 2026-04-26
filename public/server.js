06ac637e2de123f60561f559defe0e8077a7aec5
archiejohn990-coder <archiejohn990@gmail.com>
2026-04-26 20:49:28 +0800

    Update server.js

--- public/server.js
+++ public/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
