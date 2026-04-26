66ac35b34d859264020b15849b7b2b7df79182f1
archiejohn990-coder <archiejohn990@gmail.com>
2026-04-26 23:34:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,40 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const User = mongoose.model("User", userSchema);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect("mongodb://atlas-sql-69ee08b8a25dbc0359f74e25-gw0snx.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 // serve frontend
 app.use(express.static(path.join(__dirname, "public")));
